06e9858d4e44b425d410001c06a654fd5b60a85d
Polina Shebysheva <gatsbysober@gmail.com>
2022-12-06 21:23:10 +0300

    edit reformate code

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
 
-    int balance = 150;
-    int replenishment = 200;
-    if (replenishment > 1000) {
-        balance+= replenishment/100+ replenishment;
-    } else {
-        balance+=replenishment;
+        int balance = 150;
+        int replenishment = 2000;
+        if (replenishment > 1000) {
+            balance += replenishment / 100 + replenishment;
+        } else {
+            balance += replenishment;
+        }
+
+        System.out.println("Ваш баланс: " + balance + " рублей");
+
     }
-
-    System.out.println("Ваш баланс: " + balance  + " рублей");
-
-}
 }
